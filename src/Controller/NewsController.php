857e461642009eb7820a1c300671b138b1e21b21
FoxSink <iar.karpov@gmail.com>
2022-03-01 00:26:15 +0700

    Smart pagination controls

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -6,22 +6,25 @@ namespace App\Controller;
 
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class NewsController extends AbstractController
 {
+    const COUNT_POSTS_ON_PAGE = 3;
     /**
      * @throws \Exception
      */
-    public function indexAction(PostRepository $repository, ?int $page = null): Response
+    public function indexAction(PostRepository $repository, Request $request, int $page = 1): Response
     {
-        $page = $page ?? 0;
-        $paginator = $repository->getPostsPagination($page);
-
+        $queryOrder = $request->query->get('order');
+        $order = in_array($queryOrder, PostRepository::POST_ORDER_ARRAY) ? $queryOrder : PostRepository::POST_ORDER_ASC;
+        $paginator = $repository->getPostsPagination($page - 1, self::COUNT_POSTS_ON_PAGE, $order);
         return $this->render('news/index.html.twig', [
             'paginator'  => $paginator,
-            'pageCount'  => count($paginator),
+            'pageCount'  => $paginator->count()/self::COUNT_POSTS_ON_PAGE,
             'pageNumber' => $page,
+            'order'      => $order,
         ]);
     }
 
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -11,15 +11,24 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class PostRepository extends ServiceEntityRepository
 {
+    const POST_ORDER_DESC = 'DESC';
+    const POST_ORDER_ASC = 'ASC';
+
+    const POST_ORDER_ARRAY = [
+        self::POST_ORDER_DESC,
+        self::POST_ORDER_ASC,
+    ];
+
     public function __construct(ManagerRegistry $registry, string $entityClass = Post::class)
     {
         parent::__construct($registry, $entityClass);
     }
 
-    public function getPostsPagination(int $page, int $maxResults = 3):Paginator
+    public function getPostsPagination(int $page, int $maxResults, string $order = self::POST_ORDER_ASC):Paginator
     {
         $qb = $this->createQueryBuilder('p');
         $qb
+            ->orderBy('p.id', $order)
             ->setFirstResult($page * $maxResults)
             ->setMaxResults($maxResults)
         ;
